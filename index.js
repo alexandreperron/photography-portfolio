ac77be6e686b4285b03d854d2357a2a8a61acaa1
Alexandre Perron <alexandreperron@MacBook-Pro-2.local>
2025-11-23 08:36:03 -0500

    pagination

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log('Hi! Hope you are enjoying my portfolio.');
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log('Hi! Hope you are enjoying my portfolio.');
+
+    const photos = document.querySelectorAll("#gallery img");
+    const totalPages = Math.max(...[...photos].map(p => parseInt(p.dataset.page)));
+    let currentPage = 1;
+
+    function showPage(page) {
+        photos.forEach(img => {
+            img.style.display = img.dataset.page == page ? "block" : "none";
+        });
+
+        document.getElementById("pageNumber").textContent = `page ${page} / ${totalPages}`;
+        document.getElementById("prevPage").disabled = page === 1;
+        document.getElementById("nextPage").disabled = page === totalPages;
+    }
+
+    document.getElementById("prevPage").addEventListener("click", () => {
+        if (currentPage > 1) {
+            showPage(--currentPage);
+        }
+    });
+
+    document.getElementById("nextPage").addEventListener("click", () => {
+        if (currentPage < totalPages) {
+            showPage(++currentPage);
+        }
+    });
+
+    showPage(currentPage);
+});
+
